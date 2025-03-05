4c350c8705edc726dde535b8bfb2e97efd5239ef
Joseph Neil Gapuz <josephneil873@gmail.com>
2025-03-05 20:52:41 +0800

    rename random character generator folder name

---
+++ armada-comment-format.js
@@ -0,0 +1,22 @@
+//Sample function comment
+ 
+/**
+
+* DOCU: [Briefly describe the purpose of the function]
+
+* @param [data type] paramName - [Short description of the parameter's role]
+
+* @return [data type] - [Describe what the function returns, if applicable]
+
+* @throws [Exception] - [If the function throws exceptions, mention them]
+
+* 
+
+* Last Updated: [YYYY-MM-DD]
+
+* Author: [Your Name]
+
+* Last Updated By: [Updater's Name]
+
+*/
+ 
